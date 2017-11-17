d8236800b36a3b629f956f828affa67dc50285c6
agory <gory.alexandre02@gmail.com>
2017-11-17 15:08:17 +0100

    update user auth

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-
-    'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
-
+    'secret': 'cowboybepopestmoinsbienquesteinsgate',
+    'database': 'mongodb://localhost:27017/dev'
 };
