05f9351049662e37c6ce5b54cce5c71a073f66ea
GoryAlexandre <gory.alexandre02@gmail.com>
2017-11-17 15:13:21 +0100

    update user auth
    * update user auth
    
    * travis update

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 let express = require('express');
 let app = express();
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let config = require('./config');
+
 let port = process.env.PORT || 3000;
+
+mongoose.connect(config.database, {
+    useMongoClient: true
+});
+app.set('JWTSecret', config.secret);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
 let users = require('./src/route/user');
 let books = require('./src/route/book');
 app.listen(port);
@@ -8,3 +23,5 @@ app.listen(port);
 app.use('/users', users);
 app.use('/books', books);
 console.log('RESTful API server started on: ' + port);
+
+module.exports = app;
