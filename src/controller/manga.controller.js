d660767851728f3833a2d35c2b7bd50a3532c911
NicolasDiab <258147369aA>
2017-12-01 11:29:41 +0100

    first json transformation test

--- src/controller/manga.controller.js
+++ src/controller/manga.controller.js
@@ -1,30 +1,10 @@
 let Manga = require('../model/manga');
 let app = require('../../app');
 let mangaApi = require('../api/manga.api');
-let palmerabollo = require('node-isbn');
+//let palmerabollo = require('node-isbn');
 
-let daotodto = (manga) => {
-    return {
-        isbn: manga._id,
-        name: manga.name,
-    }
-};
-
-let dtotodao = (manga) => {
-    return {
-        _isbn: manga.isbn,
-        name: manga.name,
-    }
-};
 
 let MangaController = {
-    getAllmangas: function (req, res) {
-        res.status(418 || 501).json({message: 'I’m a teapot'})
-    },
-
-    getMangaByName: function (req, res) {
-        res.status(500).json(daotodto(req.manga));
-    },
 
     getMangaByIsbnApiCall: function(req, res, next) {
         let mangaIsbn = req.params.isbn;
@@ -35,7 +15,7 @@ let MangaController = {
                 return res.json({error: 'no manga with such isbn : ' + mangaIsbn, err:e});
             } else {
                 res.status(200);
-                return res.json(body);
+                return res.json(Manga(body));
             }
         });
     },
--- src/model/manga.js
+++ src/model/manga.js
@@ -1,18 +1,23 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
+let Manga = (googleApiBody) => {
+    return {
+        isbn: googleApiBody.items[0].industryIdentifiers.identifier,
+        retailPrice: {
+            amount: googleApiBody.items[0].saleInfo.retailPrice.amount,
+            currencyCode: googleApiBody.items[0].saleInfo.retailPrice.currencyCode,
+        },
+        pgaCount: googleApiBody.items[0].volumeInfo.pageCount,
+        title: googleApiBody.items[0].volumeInfo.title,
+        subtitle: googleApiBody.items[0].volumeInfo.subtitle,
+        authors: googleApiBody.items[0].volumeInfo.authors,
+        publisher: googleApiBody.items[0].volumeInfo.publisher,
+        publishedDate: googleApiBody.items[0].volumeInfo.publishedDate,
+        description: googleApiBody.items[0].volumeInfo.description,
+        imageLinks: {
+            smallThumbnail: googleApiBody.items[0].volumeInfo.imageLinks.smallThumbnail,
+            thumbnail: googleApiBody.items[0].volumeInfo.imageLinks.thumbnail,
+        },
+    }
+};
 
-var MangaSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    isbn: {
-        type: String,
-        required: true,
-        unique: "the isbn must be unique"
-    },
-
-});
-
-module.exports = mongoose.model('Mangas', MangaSchema);
+module.exports = Manga;
